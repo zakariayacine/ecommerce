4895068c2ae13c456fd713c248663ec56880fd89
zakariayacine <zakaria-yacine@hotmail.com>
2022-06-05 13:17:04 +0100

    adding products and users api

--- nodeserver/model/products.js
+++ nodeserver/model/products.js
@@ -1,14 +1,80 @@
-const database = require('./database')
+const database = require("./database");
 
-let get_services = function (callback){
-
-    let sql = 'select * from services';
-    database.connection.query(sql,null,function (err,result,fields){
-
-        if(err) console.log('error : ',err);
-
-        callback(err,result)
-
-    });
-}
-exports.get_services = get_services;
+let get_products = function (callback) {
+  let sql = "select * from products";
+  database.connection.query(sql, null, function (err, result, fields) {
+    if (err) console.log("error : ", err);
+    callback(err, result);
+  });
+};
+let get_product = function (id, callback) {
+  let sql = "select * from products where id=" + id;
+  database.connection.query(sql, null, function (err, result, fields) {
+    if (err) console.log("error : ", err);
+    callback(err, result);
+  });
+};
+let product_add = function (
+  name,
+  description,
+  photos,
+  price,
+  stock,
+  created_at,
+  updated_at,
+  callback
+) {
+  let data_insert = [
+    name,
+    description,
+    photos,
+    price,
+    stock,
+    created_at,
+    updated_at,
+  ]; //une seule insertion
+  let sql =
+    "insert into products" +
+    "(name, description, photos, price, stock, created_at, updated_at) " +
+    "values ?";
+  database.connection.query(
+    sql,
+    [[data_insert]],
+    function (result, err, fields) {
+      if (err) console.log("error : ", err);
+      console.log(result);
+      callback(err, result);
+    }
+  );
+};
+let product_update = function (
+  id,
+  name,
+  description,
+  photos,
+  price,
+  stock,
+  created_at,
+  updated_at,
+  callback
+) {
+  let data_update = {
+    id: id,
+    name: name,
+    description: description,
+    photos: photos,
+    price: price,
+    stock: stock,
+    created_at: created_at,
+    updated_at: updated_at,
+  };
+  let sql = "update products set ?";
+  database.connection.query(sql, [data_update], function (result, err, fields) {
+    if (err) console.log("error : ", err);
+    callback(err, result);
+  });
+};
+exports.get_products = get_products;
+exports.get_product = get_product;
+exports.product_add = product_add;
+exports.product_update = product_update;
--- nodeserver/routes/api.js
+++ nodeserver/routes/api.js
@@ -7,23 +7,38 @@ const products_module = require("../model/products");
 // MY MODULES
 api.get("/users", function (req, res) {
   users_module.get_users(function (err, result) {
-      let dataSent = {
-        data: {
-          users: result,
-        },
-        err: err,
-      };
-
-      res.status(200).send(dataSent);
-    });
+    let dataSent = {
+      data: {
+        users: result,
+      },
+      err: err,
+    };
+    res.status(200).send(dataSent);
   });
+});
+api.get("/user/:id", function (req, res) {
+  users_module.get_user(req.params.id, function (err, result) {
+    let dataSent = {
+      data: {
+        user: result,
+      },
+      err: err,
+    };
+    res.status(200).send(dataSent);
+  });
+});
+//continuer a finir les api
 api.post("/user/add", function (req, res) {
   let inputs = req.body;
   console.log(inputs);
-  let { nom, prenom} = inputs;
-  users_module.users_add(
-    nom,
-    prenom,
+  let { name, email, role, password, created_at, updated_at } = inputs;
+  users_module.user_add(
+    name,
+    email,
+    role,
+    password,
+    created_at,
+    updated_at,
     function (err, data) {
       if (err) console.log(err);
       let obj = {
@@ -34,5 +49,50 @@ api.post("/user/add", function (req, res) {
     }
   );
 });
-
+api.get("/products", function (req, res) {
+  products_module.get_products(function (err, result) {
+    let dataSent = {
+      data: {
+        products: result,
+      },
+      err: err,
+    };
+    res.status(200).send(dataSent);
+  });
+});
+api.get("/product/:id", function (req, res) {
+  products_module.get_product(req.params.id, function (err, result) {
+    let dataSent = {
+      data: {
+        product: result,
+      },
+      err: err,
+    };
+    res.status(200).send(dataSent);
+  });
+});
+//continuer a finir les api
+api.post("/product/add", function (req, res) {
+  let inputs = req.body;
+  console.log(inputs);
+  let { name, description, photos, price, stock, created_at, updated_at } =
+    inputs;
+  products_module.product_add(
+    name,
+    description,
+    photos,
+    price,
+    stock,
+    created_at,
+    updated_at,
+    function (err, data) {
+      if (err) console.log(err);
+      let obj = {
+        err: err,
+        data: data,
+      };
+      res.status(200).send(obj);
+    }
+  );
+});
 module.exports = api;
